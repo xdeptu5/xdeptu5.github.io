dfeb0358e066884624866d66b611fe458e81d704
xdeptu5 <107370153+xdeptu5@users.noreply.github.com>
2024-11-06 14:31:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('https://xdeptu5.github.io/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker зарегистрирован:', registration);
-      })
-      .catch((error) => {
-        console.log('Ошибка при регистрации Service Worker:', error);
-      });
-  });
-}
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('site-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
